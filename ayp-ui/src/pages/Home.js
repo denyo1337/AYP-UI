687ae57d9083130e45a75658a74893e4c721ec69
denyo1337 <63121548+denyo1337@users.noreply.github.com>
2022-05-05 01:08:47 +0200

    Code formater

--- ayp-ui/src/pages/Home.js
+++ ayp-ui/src/pages/Home.js
@@ -1,18 +1,17 @@
 import { useEffect } from 'react'
-import { Container } from '@material-ui/core'
+import { Container, Typography } from '@material-ui/core'
 import useAxios from '../hooks/useAxios'
 import { useAuthContext } from '../hooks/useAuthContext'
 import FriendWindow from './FriendsList/FriendWindow'
 
 export default function Home() {
-
   const { jwtToken, dispatch } = useAuthContext()
-  const {axiosInstance : axios} = useAxios();
+  const { axiosInstance: axios } = useAxios();
 
   useEffect(() => {
-    if(jwtToken){
+    if (jwtToken) {
       axios.get("Account").then((resp) => {
-        dispatch({type:"UPDATE_USER_STEAMDATA", payload: resp.data})
+        dispatch({ type: "UPDATE_USER_STEAMDATA", payload: resp.data })
       }, (err) => {
         console.log(err);
       })
@@ -22,8 +21,19 @@ export default function Home() {
   return (
 
     <Container >
-
-
+      <Typography variant='h1' color='primary'>
+        WELCOME TO AYP CS GO!
+      </Typography>
+      <div style={{
+        marginTop: '150px'
+      }}>
+        <Typography variant='h4' color='textSecondary' style={{
+          margin: 'auto'
+        }}>
+          App is made for simpilified view of your game performance. Create account, provide your private "SteamID"
+          and have a look at your / yours friend performance !
+        </Typography>
+      </div>
     </Container>
   )
 }
